244966dd70527a1508b37c6dbee32bbe50e1a2ae
francy10MP <francesca.precious.ricci@gmail.com>
2018-06-23 17:34:23 +0200

    xxx

--- app/src/main/java/com/uniroma2/mobynet/marvel_androidproject/MainActivity.java
+++ app/src/main/java/com/uniroma2/mobynet/marvel_androidproject/MainActivity.java
@@ -2,12 +2,28 @@ package com.uniroma2.mobynet.marvel_androidproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnCreator;
+    private Button btnCharacter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCreator= (Button) findViewById(R.id.btnCreator);
+        btnCharacter=(Button) findViewById(R.id.btnCharacter);
+        btnCreator.setOnClickListener(this);
+        btnCharacter.setOnClickListener(this);
+
+
+    }
+
+    public void onClick(View view){
+        startActivity(new Intent(MainActivity.this,SearchCreatorActivity.class));
     }
 }
