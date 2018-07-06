a9eb50c33fc2f219d288fd9a26fbd023c427ceea
Allyjuke96 <claudia.melistonti@gmail.com>
2018-07-06 15:12:21 +0200

    last activity

---
+++ app/src/main/java/com/uniroma2/mobynet/marvel_androidproject/activity/activity_element_form.java
@@ -0,0 +1,15 @@
+package com.uniroma2.mobynet.marvel_androidproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uniroma2.mobynet.marvel_androidproject.R;
+
+public class activity_element_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_element_form);
+    }
+}
